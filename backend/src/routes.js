a06d3aeda96605319e4e9b0c41538a26e7b95010
guuhx97 <gustavorobertotb@outlook.com>
2020-02-03 22:05:30 -0300

    :construction_worker: Feat: Finished listing providers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ import authMiddleware from './app/middleware/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,7 +17,10 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
