2bf9bb60925087fb6db834d1a97df16eefe29829
guuhx97 <gustavorobertotb@outlook.com>
2020-02-03 22:18:06 -0300

    :pencil2: Feat: Started a new controller and model at Appointment

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,5 +23,7 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
