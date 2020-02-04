f806434d22a6e2993362a17f2a85c072501545c9
guuhx97 <gustavorobertotb@outlook.com>
2020-02-03 21:16:25 -0300

    :card_file_box: Feat: Added file uploads and modified DB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middleware/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middleware/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,4 +18,5 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
