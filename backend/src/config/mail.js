ab8539b46fc44fb7b302db9ec89294253c114d04
guuhx97 <gustavorobertotb@outlook.com>
2020-02-06 22:06:07 -0300

    :ambulance: Feat: Created file .env for key phrese

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '3c44730860e779',
-    pass: '43020dd289a00a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
